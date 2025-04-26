26e5357ffa5f831f290c412275cc8c28df950cfb
daniapn <daniapnn@gmail.com>
2025-04-27 00:26:43 +0700

    Create create.php

---
+++ view/create.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tambah Menu</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <h1>Tambah Menu</h1>
+    <form method="POST">
+        <label>Nama:</label>
+        <input type="text" name="name" required>
+        <br>
+        <label>Harga:</label>
+        <input type="number" name="price" required>
+        <br>
+        <button type="submit">Simpan</button>
+    </form>
+    <a class="back-link" href="index.php">Kembali</a>
+</body>
+</html>
